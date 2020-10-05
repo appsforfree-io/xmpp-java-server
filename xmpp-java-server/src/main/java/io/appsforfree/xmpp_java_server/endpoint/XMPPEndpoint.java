d3a32399765b203b87f7f0c1cb20bcc33a0fbd42
Jeremy Oddos <j.oddos@useradgents.com>
2020-10-05 10:24:29 +0200

    [WebSocket] Cleaned the pom and the unneeded files

--- xmpp-java-server/src/main/java/io/appsforfree/xmpp_java_server/endpoint/XMPPEndpoint.java
+++ xmpp-java-server/src/main/java/io/appsforfree/xmpp_java_server/endpoint/XMPPEndpoint.java
@@ -2,7 +2,6 @@ package io.appsforfree.xmpp_java_server.endpoint;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServlet;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -11,25 +10,29 @@ import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/xmpp")
-public class XMPPEndpoint extends HttpServlet {
+public class XMPPEndpoint {
  
     @OnOpen
     public void onOpen(Session session) throws IOException {
         // Get session and WebSocket connection
+    	System.out.println("Connected !");
     }
  
     @OnMessage
-    public void onMessage(Session session) throws IOException {
+    public void onMessage(Session session, String message) throws IOException {
         // Handle new messages
+    	System.out.println("Message received : " + message);
     }
  
     @OnClose
     public void onClose(Session session) throws IOException {
         // WebSocket connection closes
+    	System.out.println("Disconnected !");
     }
  
     @OnError
     public void onError(Session session, Throwable throwable) {
         // Do error handling here
+    	System.out.println("Error !");
     }
 }
